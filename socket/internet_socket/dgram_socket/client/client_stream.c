ece8a63d4e1049f1eb6da92ffb979eee74a07467
LooOnGit <ironman242k2@gmail.com>
2026-05-10 15:37:08 +0700

    Add code internet socket dgram

--- socket/internet_socket/dgram_socket/client/client_stream.c
+++ socket/internet_socket/dgram_socket/client/client_stream.c
@@ -1,62 +1,60 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <sys/un.h>
-#include <errno.h>
+#include <stdio.h>    // printf, fgets, perror
+#include <stdlib.h>   // exit, EXIT_FAILURE
+#include <string.h>   // memset, strlen
+#include <unistd.h>   // close, read, write
+#include <sys/socket.h> // socket, bind, sendto, recvfrom, sockaddr
+#include <errno.h>    // errno
 
-#define SV_SOCK_PATH "/tmp/unix.dgram.sock"
-#define BUF_SIZE 4096
+#include <netinet/in.h>  // struct sockaddr_in, INADDR_ANY, htons, htonl
+#include <arpa/inet.h>   // inet_pton, inet_ntop, ntohs, ntohl
 
-int main() {
-    int clientFd = socket(AF_UNIX, SOCK_DGRAM, 0);
+#define PORT 8080
+
+int main(){
+    int clientFd = socket(AF_INET, SOCK_DGRAM, 0);
     if(clientFd < 0)
     {
         perror("socket");
         exit(EXIT_FAILURE);
     }
 
-    char clientPath[128];
-    snprintf(clientPath, sizeof(clientPath), "/tmp/unix.dgram.%d", getpid());
-    
-    unlink(clientPath);
+    struct sockaddr_in serverAddr;
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_port = htons(PORT);
+    inet_pton(AF_INET, "127.0.0.1", &serverAddr.sin_addr);
 
-    struct sockaddr_un addr;
-    memset(&addr, 0, sizeof(struct sockaddr_un));
-    addr.sun_family = AF_UNIX;
-    strncpy(addr.sun_path, clientPath, sizeof(addr.sun_path)-1);
+    char buff[1024];
+    int len;
 
-    if(bind(clientFd, (struct sockaddr *)&addr, sizeof(struct sockaddr_un)) < 0)
-    {
-        perror("bind");
-        exit(EXIT_FAILURE);   
-    }
+    struct sockaddr_in clientAddr;
+    socklen_t clientAddrLen = sizeof(clientAddr);
 
-    char buffer[BUF_SIZE];
-    memset(buffer, 0, BUF_SIZE);
-
-    struct sockaddr_un serverAddr;
-    memset(&serverAddr, 0, sizeof(struct sockaddr_un));
-    serverAddr.sun_family = AF_UNIX;
-    strncpy(serverAddr.sun_path, SV_SOCK_PATH, sizeof(serverAddr.sun_path)-1);
-    
     while(1)
     {
-        printf("Enter messages: ");
-        fgets(buffer, BUF_SIZE, stdin);
-        sendto(clientFd, buffer, strlen(buffer), 0, (struct sockaddr *)&serverAddr, sizeof(struct sockaddr_un));
-        printf("Send message\n");
-        memset(buffer, 0, BUF_SIZE);
-        socklen_t addrlen = sizeof(struct sockaddr_un);
-        int ret = recvfrom(clientFd, buffer, BUF_SIZE-1, 0, NULL, NULL);
-        if(ret < 0)
+        printf("Enter message: ");
+        fgets(buff, sizeof(buff), stdin);
+
+        if(strlen(buff) > 0)
+        {
+            len = sendto(clientFd, buff, strlen(buff), 0, (struct sockaddr*)&serverAddr, sizeof(serverAddr));
+            if(len < 0)
+            {
+                perror("sendto");
+                exit(EXIT_FAILURE);
+            }            
+        }
+
+        len = recvfrom(clientFd, buff, sizeof(buff), 0, (struct sockaddr*)&serverAddr, &clientAddrLen);
+        if(len < 0)
         {
             perror("recvfrom");
             exit(EXIT_FAILURE);
         }
-        printf("Receive: %s\n", buffer);
+
+        buff[len] = '\0';
+        printf("Server received: %s\n", buff);
     }
     close(clientFd);
-    exit(EXIT_SUCCESS);
+    return 0;
 }
--- socket/internet_socket/dgram_socket/server/server_stream.c
+++ socket/internet_socket/dgram_socket/server/server_stream.c
@@ -1,62 +1,62 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include <unistd.h>
-#include <sys/socket.h>
-#include <sys/un.h>
-#include <errno.h>
+#include <stdio.h>    // printf, fgets, perror
+#include <stdlib.h>   // exit, EXIT_FAILURE
+#include <string.h>   // memset, strlen
+#include <unistd.h>   // close, read, write
+#include <sys/socket.h> // socket, bind, sendto, recvfrom, sockaddr
+#include <errno.h>    // errno
 
-#define SV_SOCK_PATH "/tmp/unix.dgram.sock"
-#define BUF_SIZE 4096
+#include <netinet/in.h>  // struct sockaddr_in, INADDR_ANY, htons, htonl
+#include <arpa/inet.h>   // inet_pton, inet_ntop, ntohs, ntohl
 
-int main() {
-    char buffer[BUF_SIZE];
+#define PORT 8080
 
-    int serverFd = socket(AF_UNIX, SOCK_DGRAM, 0);
-    if(serverFd < 0){
+int main(int argc, char *argv[]){
+    int serverFd = socket(AF_INET, SOCK_DGRAM, 0);
+    if(serverFd < 0)
+    {
         perror("socket");
         exit(EXIT_FAILURE);
     }
 
-    unlink(SV_SOCK_PATH);
-    
-    struct sockaddr_un addr;
-    struct sockaddr_un clientAddr;
-    memset(&addr, 0, sizeof(struct sockaddr_un));
-    addr.sun_family = AF_UNIX;
-    strncpy(addr.sun_path, SV_SOCK_PATH, sizeof(addr.sun_path)-1);
-    if(bind(serverFd, (struct sockaddr *)&addr, sizeof(struct sockaddr_un)) < 0)
+    struct sockaddr_in serverAddr;
+    memset(&serverAddr, 0, sizeof(serverAddr));
+    serverAddr.sin_family = AF_INET;
+    serverAddr.sin_addr.s_addr = htonl(INADDR_ANY);
+    serverAddr.sin_port = htons(PORT);
+
+    if(bind(serverFd, (struct sockaddr *)&serverAddr, sizeof(serverAddr)))
     {
         perror("bind");
         exit(EXIT_FAILURE);
     }
 
-    printf("DGRAM server waiting...\n");
+    struct sockaddr_in clientAddr;
+    memset(&clientAddr, 0, sizeof(clientAddr));
+    socklen_t clientAddrLen = sizeof(clientAddr);
+
+    char buff[1024];
 
     while(1)
     {
-        socklen_t addrlen = sizeof(struct sockaddr_un);
-        memset(buffer, 0, BUF_SIZE);
-        int ret = recvfrom(serverFd, buffer, BUF_SIZE-1, 0, (struct sockaddr *)&clientAddr, &addrlen);
-        if(ret < 0)
+        int len = recvfrom(serverFd, buff, sizeof(buff), 0, (struct sockaddr*)&clientAddr, &clientAddrLen);
+        if(len < 0)
         {
-            perror("recv");
+            perror("recvfrom");
             exit(EXIT_FAILURE);
         }
-        printf("Receive: %s\n", buffer);
 
-        char okBuf[] = "OK\n";
-        ret = sendto(serverFd, okBuf, strlen(okBuf), 0, (struct sockaddr *)&clientAddr, sizeof(struct sockaddr_un));
-        if(ret < 0)
+        buff[len] = '\0';
+        char clientIP[INET_ADDRSTRLEN];
+        inet_ntop(AF_INET, &clientAddr.sin_addr, clientIP, sizeof(clientIP));
+        printf("Client %s:%d received: %s\n", clientIP, ntohs(clientAddr.sin_port), buff);
+
+        char okBuff[] = "OK";
+        if(sendto(serverFd, okBuff, strlen(okBuff), 0, (struct sockaddr*)&clientAddr, clientAddrLen) < 0)
         {
             perror("sendto");
             exit(EXIT_FAILURE);
         }
-        printf("Send: %s\n", okBuf);
     }
-
     close(serverFd);
-
-    exit(EXIT_SUCCESS);
-
+    return 0;
 }
